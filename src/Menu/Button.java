039fffc941572e3dc445406383e0b764a4ee7271
aswartz <student@cch-w431-imac02.ccsd.d5>
2015-04-24 13:58:44 -0600

    buttons light up

--- src/Menu/Button.java
+++ src/Menu/Button.java
@@ -13,25 +13,45 @@ import org.lwjgl.opengl.GL11;
 import framework.Draw;
 import framework.Game;
 
+import org.newdawn.slick.opengl.Texture;
+
 public class Button {
 	private float x,y,width,height;
+	private Texture button;
+	private String folder;
 	
-	public Button(float x, float y, float w, float h){
+	public Button(float x, float y, float w, float h, String f){
 		this.x = x;
 		this.y = y;
 		width = w;
 		height = h;
+		folder = f;
+		button = Draw.quickLoad(folder+"/sprite_"+1);
 	}
 	
+	public void setButton(int i){
+		button = Draw.quickLoad(folder+"/sprite_"+i);
+	}
+	public void hover(){
+		System.out.println(x);
+		System.out.println(Mouse.getX());
+		if(Mouse.getX()>=x+70 && Mouse.getX()<=x+170 && Mouse.getY()>=y+100 && Mouse.getY()<=y+200)
+			setButton(2);
+		else
+			setButton(1);
+	}
 	public boolean isPressed(){
-		if(Mouse.getX()>=x && Mouse.getX()<=x+width && Mouse.getY()>=y+height/2 && Mouse.getY()<=y+3*height/2)
+		if(Mouse.getX()>=x+70 && Mouse.getX()<=x+width-120 && Mouse.getY()>=y+2*height && Mouse.getY()<=y+2*height+100)
 			if(Mouse.isButtonDown(0))
 				return true;
 		return false;
 	}
 	
 	public void drawButton(){
-		Draw.drawQuad(x, y, width, height);
-
+		glEnable(GL_BLEND); //ENABLES BLEND FOR TRANSPARENCY
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); //SETS THE BLEND FUNCTION TO WORK PROPERLY -- EVERYTHING BETWEEN HERE
+		Draw.drawQuad(x, y, width, height,button); //Draw the player based on current position and current animation.
+		glDisable(GL_BLEND); // DISABLES BLEND FUNCTION ------------------------------------------------ AND HERE USES TRANSPARENCY.
+		hover();
 	}
 }
--- src/Menu/MenuScreen.java
+++ src/Menu/MenuScreen.java
@@ -12,17 +12,16 @@ public class MenuScreen {
 	private Button exit;
 	
 	public MenuScreen(){
-		play = new Button(490,350,300,150);
-		exit = new Button(490,150,300,150);
+		play = new Button(490,250,300,150,"play");
+		//exit = new Button(490,150,300,150,"play");
 	}
 	
 	public void drawMenu(){
 		Draw.drawQuad(0, 0, 1280, 720,0,0,0);
 		play.drawButton();
-		exit.drawButton();
+		//exit.drawButton();
 	}
 	public void start() {
-		Draw.Setup();
          
         // init OpenGL here
          
@@ -33,13 +32,14 @@ public class MenuScreen {
             Display.update();
             Display.sync(60);
             System.out.println("Play: " + play.isPressed());
-            System.out.println("Exit: " + exit.isPressed());
+            //System.out.println("Exit: " + exit.isPressed());
         }
          
         Display.destroy();
     }
      
     public static void main(String[] args) {
+    	Draw.Setup();
         MenuScreen displayExample = new MenuScreen();
         displayExample.start();
         
